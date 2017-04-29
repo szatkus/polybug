b5dc87f7c15ed244e1b9f36aa745c2b2359a5ffa
szatkus <tomasz.szatkowski@baroo.ie>
2017-04-29 21:53:20 +0200

    Add tests and fix deactivation

--- spec/simple-spec.js
+++ spec/simple-spec.js
@@ -5,12 +5,12 @@ import polybug from '../lib/polybug';
 describe('Simple case', () => {
   beforeEach(() => {
     atom.packages.activatePackage('polybug');
-  });
-  it('Open correct file and debug', () => {
     //TODO: find a more portable workaround
     waitsForPromise(() => atom.workspace.open('/home/szatkus/polybug/spec/simple.py'));
-
     waitsFor(() => atom.workspace.getActiveTextEditor());
+  });
+
+  it('Open correct file and exit', () => {
     runs(() => {
       let workspaceElement = atom.views.getView(atom.workspace);
       atom.commands.dispatch(workspaceElement, 'polybug:debug');
@@ -18,13 +18,52 @@ describe('Simple case', () => {
       runs(() => {
         let output = workspaceElement.querySelector('.polybug-panel .output');
         let lines = output.innerHTML.split('\n');
-        console.log(lines);
         expect(lines[1]).toEqual('-&gt; print("test")');
         let input = workspaceElement.querySelector('.polybug-panel input');
         input.value = 'exit';
         let event = new KeyboardEvent('keypress', {key: 'Enter'});
         input.dispatchEvent(event);
         waitsFor(() => input.readOnly);
+      });
+    });
+  });
+
+  it('Open correct file and exit', () => {
+    runs(() => {
+      let workspaceElement = atom.views.getView(atom.workspace);
+      atom.commands.dispatch(workspaceElement, 'polybug:debug');
+      waitsFor(() => workspaceElement.querySelector('.polybug-panel .output').innerHTML);
+      runs(() => {
+        let output = workspaceElement.querySelector('.polybug-panel .output');
+        let lines = output.innerHTML.split('\n');
+        expect(lines[1]).toEqual('-&gt; print("test")');
+        let input = workspaceElement.querySelector('.polybug-panel input');
+        input.value = 'exit';
+        let event = new KeyboardEvent('keypress', {key: 'Enter'});
+        input.dispatchEvent(event);
+        waitsFor(() => input.readOnly);
+      });
+    });
+  });
+
+  it('Open a file and step in', () => {
+
+    runs(() => {
+      let workspaceElement = atom.views.getView(atom.workspace);
+      atom.commands.dispatch(workspaceElement, 'polybug:debug');
+      waitsFor(() => workspaceElement.querySelector('.polybug-panel .output').innerHTML);
+      runs(() => {
+        let output = workspaceElement.querySelector('.polybug-panel .output');
+        let lines = output.innerHTML.split('\n');
+        expect(lines[1]).toEqual('-&gt; print("test")');
+        let button = workspaceElement.querySelectorAll('.polybug-panel .btn')[3];
+        let event = new MouseEvent('click');
+        button.dispatchEvent(event);
+        waitsFor(() => output.innerHTML.split('\n').length > 4);
+        runs(() => {
+          let lines = output.innerHTML.split('\n');
+          expect(lines[3]).toEqual('test');
+        });
       });
     });
   });
